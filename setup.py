7680f30a6b3f43f3ac21f1eebd4c43a1453bcc1f
Amagash <tiffanysouterre@gmail.com>
2020-10-20 21:06:57 +0000

    update v1.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='hacktoberfest_jems',
     author='Tiffany Souterre',
-    version='1.0.2',
+    version='1.0.3',
     packages=['hacktoberfest_jems'],
     description="An application to celebrate hacktoberfest!",
     include_package_data=True,
