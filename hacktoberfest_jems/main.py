e51ed2edfa9d41f95e7a506ec09d16bb787ee6be
damien.d.michelle <damien.d.michelle@renault.com>
2021-03-09 19:42:14 +0100

    make PEP 8 convention great again

--- hacktoberfest_jems/main.py
+++ hacktoberfest_jems/main.py
@@ -2,15 +2,17 @@ import random
 import json
 import pkgutil
 
+
 def happy_hacktoberfest():
     test = pkgutil.get_data(__name__, "data/contributors.json")
     data = json.loads(test)
     pick = random.choice(data)
-    sentence = "{} wishes you {}".format(pick["author"], pick["wish"])
-    return sentence
+    return "{} wishes you {}".format(pick["author"], pick["wish"])
+
 
 def main():
     print(happy_hacktoberfest())
+
 
 if __name__ == "__main__":
     main()
